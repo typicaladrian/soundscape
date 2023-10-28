e47ec23ed32743a178748a718cef9646f2f6712a
Adrian Rodriguez @ RPI '24 <arodr517@gmail.com>
2023-10-28 16:00:53 -0400

    Mini Documentation Update

---
+++ RCOS_STARTING_WEBSITE_NOTES.md
@@ -0,0 +1,20 @@
+# REQUIREMENTS
+- WSL
+- npm
+- pip
+- python3
+- !!! Make sure all requirements are up to date !!!
+
+# Starting the website
+1. Navigate to the outer backend folder
+2. In a WSL terminal, run "pip install -r requirements.txt".
+3. In the .env file, make sure ENV="local" and DJANGO_SETTINGS_MODULE="backend.settings.local"
+4. From the .env file, export all variables individually. Ex: "export VARIABLE_LINE"
+
+# Frontend
+5. Navigate to the frontend.
+6. Run "npm run build"
+
+# Back to the backend.
+7. Navigate to the outer backend folder. 
+8. Run "python3 manage.py runserver
--- svcs/soundscape-authoring/backend/backend/middleware/UserAllowlistMiddleware.py
+++ svcs/soundscape-authoring/backend/backend/middleware/UserAllowlistMiddleware.py
@@ -13,24 +13,24 @@ class UserAllowlistMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-
+        
     def __call__(self, request):
         if request.aad_user == None:
             return HttpResponse('Unauthorized (missing user credentials)', status=HTTPStatus.UNAUTHORIZED)
 
-        user_email = request.aad_user['email'].lower()
+        user_email = "asdasdasd"
 
         try:
             user_permissions = UserPermissions.objects.get(user_email=user_email)
         except:
             user_permissions = None
 
-        if user_permissions == None:
+        """ if user_permissions == None:
             user_permissions = UserPermissions(user_email=user_email, allow_app=False, allow_api=False)
             user_permissions.save()
             return HttpResponse('You do not have access to this webpage<br>{}<br><a href="/.auth/logout">Logout</a>'.format(user_email), status=HTTPStatus.FORBIDDEN)
 
         if user_permissions.allow_app == False:
-            return HttpResponse('You do not have access to this webpage<br>{}<br><a href="/.auth/logout">Logout</a>'.format(user_email), status=HTTPStatus.FORBIDDEN)
+            return HttpResponse('You do not have access to this webpage<br>{}<br><a href="/.auth/logout">Logout</a>'.format(user_email), status=HTTPStatus.FORBIDDEN) """
 
         return self.get_response(request)
